5ed11402090138c635a0a65042d8a74a2292ad6b
Rafihul <rafihul2905@gmail.com>
2025-02-06 09:00:00 +0700

    setup: mainactivity and drawfragment

---
+++ app/src/main/java/com/rafih/justdraw/presentation/ui/DrawFragment.kt
@@ -0,0 +1,34 @@
+package com.rafih.justdraw.presentation.ui
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rafih.justdraw.R
+import com.rafih.justdraw.databinding.FragmentDrawBinding
+
+class DrawFragment : Fragment(R.layout.fragment_draw) {
+
+    private var _binding : FragmentDrawBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentDrawBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
